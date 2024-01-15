11afef0f4e39053f7b98d35bf7bd6c9ba30382b8
Zabdi Samuel Gomez Olvera <samyolvera1@gmail.com>
2024-01-15 17:46:03 -0500

    fixed color and added hero banner

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import LocalDiningIcon from '@mui/icons-material/LocalDining';
-type Props = {}
+import React from 'react';
+import { styled, useTheme} from '@mui/system';
+import { Typography,Container } from '@mui/material';
+import DiningIcon from '@mui/icons-material/Dining';
 
-const Header = (props: Props) => {
-  return (
-    <div className="hero-container">
-      <div className="content">
-        <h1>WDYW <span> <LocalDiningIcon/></span></h1>
-        <h2>Don't know what you want? </h2>
-        <h3> Come figure it out!</h3>
-      </div> 
-    </div>
-  )
+interface HeroBannerStyles {
+  background: string;
+  color: string;
+  padding: string | ((theme: any) => any);
+  textAlign?: React.CSSProperties['textAlign'];
 }
-export default Header
+
+const heroBannerStyles: HeroBannerStyles = {
+  background: '#F1FAEE',
+  color: '#1e81b0',
+  padding: (theme: any) => theme.spacing(6), // or '16px' or any other valid CSS padding value
+  textAlign: 'center',
+};
+
+
+
+const HeroBanner = () => (
+ <div style={heroBannerStyles as React.CSSProperties}>
+    <Container maxWidth="md">
+      <Typography variant="h2" component="div" gutterBottom sx={{ fontSize: '2.5rem' }}>
+    
+        WDYW
+        <span> <DiningIcon fontSize="large"/></span>
+      </Typography>
+
+      <Typography variant="h5" component="div" sx={{ fontSize: '1.25rem', marginTop: '1rem' }}>
+        Don't know what you want?<br />
+        Come figure it out!
+      </Typography>
+    </Container>
+    </div>
+);
+
+export default HeroBanner;
