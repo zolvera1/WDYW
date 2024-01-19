9bc754c5fa46b8d6b26782a78957dd55ef48a3db
Zabdi Samuel Gomez Olvera <samyolvera1@gmail.com>
2024-01-18 19:52:41 -0500

    changed dist name from public to dist

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.post('/api', (req, res) => {
